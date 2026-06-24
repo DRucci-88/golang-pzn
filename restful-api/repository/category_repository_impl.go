8e15466b64521efd66940e6f74b76db3a5fff42a
rucci <d.rucci.2001@gmail.com>
2026-06-24 18:13:40 +0700

    rest Repository, Service and Model Domain

---
+++ restful-api/repository/category_repository_impl.go
@@ -0,0 +1,65 @@
+package repository
+
+import (
+	"context"
+	"database/sql"
+	"errors"
+	"restful-api/helper"
+	"restful-api/model/domain"
+)
+
+type CategoryRepositoryImpl struct {
+}
+
+func (repo *CategoryRepositoryImpl) Save(ctx context.Context, tx *sql.Tx, category domain.Category) domain.Category {
+	sql := "INSERT INTO customer(name) values(?)"
+	result, err := tx.ExecContext(ctx, sql, category.Name)
+	helper.PanicIfError(err)
+
+	id, err := result.LastInsertId()
+	helper.PanicIfError(err)
+
+	category.Id = int(id)
+	return category
+}
+func (repo *CategoryRepositoryImpl) Update(ctx context.Context, tx *sql.Tx, category domain.Category) domain.Category {
+	sql := "UPDATE category set name = ? where id = ?"
+	_, err := tx.ExecContext(ctx, sql, category.Name, category.Id)
+	helper.PanicIfError(err)
+
+	return category
+}
+func (repo *CategoryRepositoryImpl) Delete(ctx context.Context, tx *sql.Tx, category domain.Category) {
+	sql := "DELETE FROM category where id = ?"
+	_, err := tx.ExecContext(ctx, sql, category.Id)
+	helper.PanicIfError(err)
+
+}
+func (repo *CategoryRepositoryImpl) FindById(ctx context.Context, tx *sql.Tx, categoryId int) (domain.Category, error) {
+	sql := "SELECT id, name FROM category WHERE id = ? LIMIT 1"
+	rows, err := tx.QueryContext(ctx, sql, categoryId)
+	helper.PanicIfError(err)
+	category := domain.Category{}
+	if rows.Next() {
+		err := rows.Scan(&category.Id, &category.Name)
+		helper.PanicIfError(err)
+		return category, nil
+	} else {
+		return category, errors.New("category is not found")
+	}
+
+}
+func (repo *CategoryRepositoryImpl) FindAll(ctx context.Context, tx *sql.Tx) []domain.Category {
+	sql := "SELECT id, name FROM category"
+	rows, err := tx.QueryContext(ctx, sql)
+	helper.PanicIfError(err)
+	var categories []domain.Category
+
+	for rows.Next() {
+		category := domain.Category{}
+		err := rows.Scan(&category.Id, &category.Name)
+		helper.PanicIfError(err)
+		categories = append(categories, category)
+	}
+	return categories
+}
---
+++ restful-api/sql/category.sql
@@ -0,0 +1,7 @@
+-- Active: 1782153380096@@localhost@3306@golang_rest_pzn
+CREATE TABLE category(
+    id INTEGER PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(200) NOT NULL
+) engine = InnoDB;
+
+SELECT * FROM category;
